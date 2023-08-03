ce783ac48414e01c1093b04f3be09fbf443c2e6e
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2023-08-03 22:02:37 +0900

    Added Center Net.

---
+++ 2023 Tsukuba Challenge/Simple CenterNet/train_voc.py
@@ -0,0 +1,33 @@
+from trainer.trainer import Trainer
+from dataset.voc import VOCDataset
+from model.centernet import CenterNet
+from config.voc import Config
+from loss.loss import Loss
+
+import torch
+from torch.utils.data import DataLoader
+
+
+def train(cfg):
+    train_ds = VOCDataset(cfg.root, mode=cfg.split, resize_size=cfg.resize_size)
+    train_dl = DataLoader(train_ds, batch_size=1, shuffle=True,
+                          num_workers=cfg.num_workers, collate_fn=train_ds.collate_fn, pin_memory=True)
+
+    model = CenterNet(cfg)
+    device = torch.device('cuda')
+
+    if cfg.gpu:
+        model = model.cuda()
+    loss_func = Loss(cfg)
+
+    epoch = 100
+    cfg.max_iter = len(train_dl) * epoch
+    cfg.steps = (int(cfg.max_iter * 0.6), int(cfg.max_iter * 0.8))
+
+    trainer = Trainer(cfg, model, loss_func, train_dl, None)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    cfg = Config
+    train(cfg)
