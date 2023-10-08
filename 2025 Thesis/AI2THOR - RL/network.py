b4d051773b47a15ec36b9b17be93c92bb35168c5
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2023-10-09 02:13:37 +0900

    Updated last training.

--- 2025 Thesis/AI2THOR - RL/network.py
+++ 2025 Thesis/AI2THOR - RL/network.py
@@ -6,11 +6,13 @@ import torch.nn.functional as F
 class DQN(nn.Module):
     """Define DQN architecture."""
     
-    def __init__(self, width, height, action_size):
+    def __init__(self, width, height, action_size, seed):
         """Initialize parameters and build model."""
             
         super(DQN, self).__init__()
         
+        self.seed = torch.manual_seed(seed)
+
         # CNN will take in the difference between the current and previous screen patches
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
