04109e37105b8d24c834baf7acdd2b6f91d9504e
Muhammad Angga Muttaqien <muha.muttaqien@gmail.com>
2023-11-07 13:36:34 +0900

    Fixed issue in multimodal state.

--- 2025 Thesis/AI2THOR - RL/utils.py
+++ 2025 Thesis/AI2THOR - RL/utils.py
@@ -55,7 +55,41 @@ def plot_durations(score, i_episode, num_episodes):
     if is_ipython and i_episode is not num_episodes:
         display.clear_output(wait=True)
         plt.show()
-    else: return
+    else: 
+        return
+
+# a helper for plotting the durations of episodes
+def plot_durations_wo_line(score, i_episode, num_episodes):
+    
+    episode_scores.append(score)
+                
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.tensor(episode_scores, dtype=torch.float)
+    
+    plt.title(f'Training Episode: {i_episode}')
+    plt.xlabel('Episode')
+    plt.ylabel('Cummulative Rewards')
+    
+    # Plot points without a line
+    plt.plot(durations_t.numpy(), 'go')  # 'go' stands for green points
+    
+    # take 100 episode averages and plot them
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy(), color='red')
+    
+    # pause a bit so that plots are updated
+    if not os.path.exists('./images/'): os.makedirs('./images/')
+    plt.savefig('./images/plot_of_training_result_AI2THOR_RL.png')
+    plt.pause(0.001)
+    
+    if is_ipython and i_episode is not num_episodes:
+        display.clear_output(wait=True)
+        plt.show()
+    else:
+        return
 
 def encode_feedback(event, controller, target_name):
     
@@ -71,4 +105,3 @@ def encode_feedback(event, controller, target_name):
         reward = -0.05
            
     return None, reward, is_found, None
-
